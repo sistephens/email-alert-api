656197e6332874a28c12ae53e282b1f0226902b3
Graham Pengelly <graham@three-tier.com>
2017-11-28 17:27:26 +0000

    Add HTML version

--- app/services/email_sender_service/send_grid_sender.rb
+++ app/services/email_sender_service/send_grid_sender.rb
@@ -4,14 +4,38 @@ class EmailSenderService
   class SendGridSender #TODO: if we name this consistently with Notify it will clash with the Sendgrid module
     include SendGrid
 
-    def call(address:, subject:, body:)
-      #TODO this would have to change as we can't use the notifications email address
-      from_address = "test@test.com"
+    class SendGridBodyConverter
+      def self.to_text(markdown)
+        <<~TEXT
+          text version
 
-      from = SendGrid::Email.new(email: from_address)
+          #{markdown}
+
+        TEXT
+      end
+
+      def self.to_html(markdown)
+        <<~HTML
+          <h1>HTML Version</h1>
+
+          <p> Parse me *************************</p>
+
+          #{markdown}
+
+          <p> **********************************</p>
+        HTML
+      end
+    end
+
+    def call(address:, subject:, body:)
+      #TODO we can't use the notifications email address
+      from_address = "test@test.com"
       to = SendGrid::Email.new(email: address)
-      content = SendGrid::Content.new(type: 'text/plain', value: body)
-      mail = Mail.new(from, subject, to, content)
+      plain_text = SendGrid::Content.new(type: 'text/plain', value: SendGridBodyConverter.to_text(body))
+      from = SendGrid::Email.new(email: from_address)
+
+      mail = Mail.new(from, subject, to, plain_text)
+      mail.add_content(SendGrid::Content.new(type: 'text/html', value: SendGridBodyConverter.to_html(body)))
 
       client.mail._('send').post(request_body: mail.to_json)
     end
