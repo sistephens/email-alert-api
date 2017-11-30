5b8913f629e21a1e44ac8b40576c928bb23c513c
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-11-30 17:55:01 +0000

    Split out the public routing to be done via constraints
    This sets a hostname match to ensure that only when this is hosted on a
    certain hostname are methods made public.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
-  resources :subscriber_lists, path: "subscriber-lists", only: [:create]
-  get "/subscriber-lists", to: "subscriber_lists#show"
+  status_updates_hostname = ENV.fetch("STATUS_UPDATES_HOSTNAME_PREFIX", "email-status-updates")
 
-  resources :notifications, only: %i[create index show]
-  resources :status_updates, path: "status-updates", only: %i[create]
+  constraints host: /^#{Regexp.escape(status_updates_hostname)}/ do
+    resources :status_updates, path: "/", only: %i[create]
+  end
 
-  get "/healthcheck", to: "healthcheck#check"
+  constraints host: /^(?!#{Regexp.escape(status_updates_hostname)})/ do
+    resources :subscriber_lists, path: "subscriber-lists", only: [:create]
+    get "/subscriber-lists", to: "subscriber_lists#show"
 
-  post "/unsubscribe/:uuid", to: "unsubscribe#unsubscribe"
+    resources :notifications, only: %i[create index show]
+    resources :status_updates, path: "status-updates", only: %i[create]
+
+    get "/healthcheck", to: "healthcheck#check"
+
+    post "/unsubscribe/:uuid", to: "unsubscribe#unsubscribe"
+  end
 end
