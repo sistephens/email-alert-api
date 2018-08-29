ff0459a83189737a5029361b4907b2f199881936
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2018-08-29 15:52:05 +0100

    Fix "Delegating exists to arel is deprecated" message

--- app/queries/content_changes_for_email_query.rb
+++ app/queries/content_changes_for_email_query.rb
@@ -1,9 +1,5 @@
 class ContentChangesForEmailQuery
   def self.call(email)
-    subscription_content = SubscriptionContent
-      .where("subscription_contents.content_change_id = content_changes.id")
-      .where(email: email)
-      .exists
-    ContentChange.where(subscription_content)
+    ContentChange.joins(:subscription_contents).where(subscription_contents: {email: email}).distinct
   end
 end
